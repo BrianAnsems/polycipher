ce06a7a05c0671fe2a5a1f0782ea1d50191ddfbd
BrianAnsems <brian@thinkingbig.net>
2022-02-02 21:22:25 -0400

    Remove console logs.

--- polycipher.js
+++ polycipher.js
@@ -7,32 +7,28 @@ function encipherText(){
 		let code = "";
 		let alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 		let cipherAlphabet = {};
-		console.log(cipherAlphabet);
 		//Give cipher Alphabet properties of each letter in the key word
 
 		for(let t=0;t<wKey.length;t++)
 		{
 			cipherAlphabet["wCipher"+t]=[];
-			console.log(cipherAlphabet["wCipher"+t]);
+
 			for(let a=0;a<alphabet.length;a++)
 			{
 				cipherAlphabet["wCipher"+t].push(alphabet[a]);
 			}
-			console.log(cipherAlphabet["wCipher"+t]);
 
 			while(cipherAlphabet["wCipher"+t][0]!=wKey.charAt(t))
 			{
 				let letter = cipherAlphabet["wCipher"+t].pop();
 				cipherAlphabet["wCipher"+t].unshift(letter);
 			}
-			console.log(cipherAlphabet["wCipher"+t][0]);
 		}
 
 		let f = 0;
 		//encipher plaintext with cipher alphabet
 		for(let i=0;i<text.length;i++)
 		{
-			console.log(f);
 			let ciphered = false;
 			if(text[i]==" ")
 			{
